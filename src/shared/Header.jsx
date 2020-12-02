8be3066979e778a47f5e88ddf09dfece28be7501
AdamabuMedina <adamabujamila@gmail.com>
2020-12-03 01:29:04 +0300

    настройка tsconfig

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -10,18 +10,3 @@ function HeaderComponent() {
 }
 
 export const Header = hot(HeaderComponent)
-// function HeaderComponent() {
-//    const [state, setState] = React.useState(false)
-//    return (
-//       <header>
-//          <h1>Hmr example</h1>
-//          <button onClick={() => setState(!state)}>
-//             {state ? "hide" : "show"}
-//          </button>
-//          {state && <h3>Here i'm</h3>}
-//          <p>
-//             It replace code without page reloading
-//          </p>
-//       </header>
-//    )
-// }
