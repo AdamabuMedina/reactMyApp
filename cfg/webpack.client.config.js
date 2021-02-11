e9c7b8270292bed51e5e047a6098c80de837d390
AdamabuMedina <adamabujamila@gmail.com>
2021-02-12 01:40:44 +0300

    Создаем Layout приложения. Часть 1

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -1,65 +1,71 @@
 const path = require("path")
-const { HotModuleReplacementPlugin } = require("webpack")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {HotModuleReplacementPlugin} = require("webpack")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 const NODE_ENV = process.env.NODE_ENV
 const IS_DEV = NODE_ENV === "development"
 const IS_PROD = NODE_ENV === "production"
+const GLOBAL_CSS_REGEXP = /\.global\.css$/
 
 function setupDevtool() {
-    if (IS_DEV) return "eval"
-    if (IS_PROD) return false
+  if (IS_DEV) return "eval"
+  if (IS_PROD) return false
 }
 
 module.exports = {
-    mode: NODE_ENV ? NODE_ENV : "development",
-    resolve: {
-        extensions: [".js", "jsx", ".json"],
-        alias: {
-            "react-dom": IS_DEV ? "@hot-loader/react-dom" : "react-dom",
-        }
-    },
-    entry: [
-        path.resolve(__dirname, "../src/client/index.jsx"),
-        "webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr",
-    ],
-    output: {
-        path: path.resolve(__dirname, "../dist/client"),
-        filename: "client.js",
-        publicPath: "/static/",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.[jt]sx?$/,
-                use: ["ts-loader"]
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    "style-loader", {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                mode: "local",
-                                localIdentName: "[name]__[local]--[hash:base64:5]",
-                            }
-                        }
-                    },
-                ]
-            },
-        ]
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        hot: IS_DEV
-    },
-    devtool: setupDevtool(),
-    plugins: IS_DEV
-        ? [
-            new CleanWebpackPlugin(),
-            new HotModuleReplacementPlugin(),
-        ]
-        : [],
+  mode: NODE_ENV ? NODE_ENV : "development",
+  resolve: {
+    extensions: [".js", "jsx", ".json", ".ts", ".tsx"],
+    alias: {
+      "react-dom": IS_DEV ? "@hot-loader/react-dom" : "react-dom",
+    }
+  },
+  entry: [
+    path.resolve(__dirname, "../src/client/index.jsx"),
+    "webpack-hot-middleware/client?path=http://localhost:3001/static/__webpack_hmr",
+  ],
+  output: {
+    path: path.resolve(__dirname, "../dist/client"),
+    filename: "client.js",
+    publicPath: "/static/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        use: ["ts-loader"]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader", {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              }
+            }
+          },
+        ],
+        exclude: GLOBAL_CSS_REGEXP
+      },
+      {
+        test: GLOBAL_CSS_REGEXP,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: IS_DEV
+  },
+  devtool: setupDevtool(),
+  plugins: IS_DEV
+    ? [
+      new CleanWebpackPlugin(),
+      new HotModuleReplacementPlugin(),
+    ]
+    : [],
 }
