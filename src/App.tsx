a9b7aba52554e5aae724241f11d40a8f39ba8d9d
AdamabuMedina <adamabujamila@gmail.com>
2021-02-12 04:31:31 +0300

    Создаем компонент новостной карточки

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import {Layout} from "./shared/LayoutApp/Layout/Layout";
 import "./main.global.css"
 import {Header} from "./shared/LayoutApp/Header/Header";
 import {Content} from "./shared/LayoutApp/Content/Content";
+import {CardList} from "./shared/LayoutApp/CardList/CardList";
 
 
 function AppComponent() {
     return (
         <Layout>
             <Header/>
-            <Content>content</Content>
+            <Content>
+                <CardList/>
+            </Content>
         </Layout>
     )
 }
