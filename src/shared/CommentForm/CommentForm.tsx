8de629220cbf4b850710711b8c09904977e924d3
AdamabuMedina <adamabujamila@gmail.com>
2021-03-15 03:42:59 +0300

    12 module homework

--- src/shared/CommentForm/CommentForm.tsx
+++ src/shared/CommentForm/CommentForm.tsx
@@ -1,58 +1,33 @@
 import React from "react";
 import styles from "./commentform.css"
+import {Form, Formik, Field} from "formik"
 
-// interface ICommentForm {
-//     myRef?: React.Ref<HTMLTextAreaElement>,
-//     uncontrolled?: boolean,
-//     mainComment?: boolean,
-//     value: string,
-//     onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void,
-//     onSubmit: (event: React.FormEvent) => void,
-// }
-// {
-//     myRef, uncontrolled=true, mainComment=false, onChange, onSubmit, value
-//     }: ICommentForm
+
+function validateComment(value: string) {
+    let error = '';
+    if (value.length <= 3) error = "Должно быть больше 3х символов!"
+    return error;
+}
 
 export function CommentForm() {
-    const [value, setValue] = React.useState("")
-    const [touched, setTouched] = React.useState(false)
-    const [valueError, setValueError] = React.useState("")
+    return (
+        <Formik
+            initialValues={{
+                commentText: '',
+            }}
+            onSubmit={values => {
+                console.log(values);
+                alert(`Отправка формы`);
+            }}
+        >
+            {({errors, touched, isValidating}) => (
+                <Form className={styles.form}>
 
-    function handleSubmit(event: React.FormEvent) {
-        event.preventDefault()
-        setTouched(true)
-        setValueError(validateValue())
+                    <Field name="comment" validate={validateComment} as='textarea' className={styles.input}/>
+                    {errors.commentText && touched.commentText && <div>{errors.commentText}</div>}
 
-        const isFormValid = !validateValue()
-        if(!isFormValid) return
-
-        alert("Форма отправлена")
-    }
-
-    function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-        setValue(event.target.value)
-    }
-
-    function validateValue() {
-        if (value.length <= 3) return "Введите больше 3- символов"
-        return ""
-    }
-
-
-        return (
-            <form className={styles.form} onSubmit={handleSubmit}>
-                <textarea
-                    className={styles.input}
-                    placeholder="Введите комментарий"
-                    onChange={handleChange}
-                    aria-invalid={valueError ? "true" : undefined}/>
-                    {
-                        touched && valueError &&
-                        (<div>{valueError}</div>)
-                    }
-                <button className={styles.button} type="submit">
-                    Комментировать
-                </button>
-            </form>
-        )
+                    <button type="submit" className={styles.button}>Комментировать</button>
+                </Form>
+            )}
+        </Formik>)
 }
--- src/shared/CommentForm/CommentFormContainer/CommentFormContainer.tsx
+++ src/shared/CommentForm/CommentFormContainer/CommentFormContainer.tsx
@@ -1,36 +1,38 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState, updateComment } from "../../../store/rootReducer";
-import { CommentForm } from "../CommentForm";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState, updateComment} from "../../../store/rootReducer";
+import {CommentForm} from "../CommentForm";
 
 export function CommentFormContainer(
     {
         myRef,
-        uncontrolled=true,
-        mainComment=false}:
-        {myRef?: React.Ref<HTMLTextAreaElement>,
-        uncontrolled?: boolean,
-        mainComment?:boolean
-    }) {
-        const value = useSelector<RootState, string>(state => state.commentText)
-        const dispatch = useDispatch()
+        uncontrolled = true,
+        mainComment = false
+    }:
+        {
+            myRef?: React.Ref<HTMLTextAreaElement>,
+            uncontrolled?: boolean,
+            mainComment?: boolean
+        }) {
+    const value = useSelector<RootState, string>(state => state.commentText)
+    const dispatch = useDispatch()
 
-        function handleSubmit(event: React.FormEvent) {
-            event.preventDefault();
-        }
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+    }
 
-        function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-            dispatch(updateComment(event.target.value))
-        }
+    function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        dispatch(updateComment(event.target.value))
+    }
 
-        return (
-           <CommentForm
+    return (
+        <CommentForm
             // value={value}
             // onChange={handleChange}
             // onSubmit={handleSubmit}
-            // myRef = {myRef}
-            // uncontrolled = {uncontrolled}
-            // mainComment  = {mainComment}
-           />
-        )
+            // myRef={myRef}
+            // uncontrolled={uncontrolled}
+            // mainComment={mainComment}
+        />
+    )
 }
